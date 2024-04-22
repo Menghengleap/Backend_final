676cb7c3f24048dea6674e5e4e059659c12f0798
menghengleap <menghengleap@yahoo.com>
2024-04-22 15:24:23 -0500

    connect

--- config/database.js
+++ config/database.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/statesDB')
-  .then(() => {
-  })
-  .catch(err => {
-    console.error("Error connecting to MongoDB:", err);
-    process.exit(1);
-  });
-
 // const mongoose = require('mongoose');
 
-// const connectDB = async () => {
-//   try{
-//       await mongoose.connect(process.env.DATABASE_URI, {
-//           useUnifiedTopology: true,
-//           useNewUrlParser: true
-//       });
-//   } catch (err){
-//       console.error(err);
-//   }
-// }
+// mongoose.connect('mongodb://127.0.0.1:27017/statesDB')
+//   .then(() => {
+//   })
+//   .catch(err => {
+//     console.error("Error connecting to MongoDB:", err);
+//     process.exit(1);
+//   });
 
-// module.exports = connectDB
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try{
+      await mongoose.connect(process.env.DATABASE_URI, {
+          useUnifiedTopology: true,
+          useNewUrlParser: true
+      });
+  } catch (err){
+      console.error(err);
+  }
+}
+
+module.exports = connectDB
