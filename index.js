a4d3d9e942d3b551d039a68a7b08757f5be5cdad
menghengleap <menghengleap@yahoo.com>
2024-04-22 15:57:07 -0500

    connect

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs'); // require the file system module
 const PORT = process.env.PORT || 3500;
 const mongoose = require('mongoose');
-const ConnectDB = require('./config/database');
+const connectDB = require('./config/database');
 const { connect } = require('http2');
 const connectDB = require('./config/database');
 
